ada08ee22c86f0442d13c4e2dc93bce728670530
Tad Preston <tad.preston@gatewaystaff.com>
2021-02-08 16:04:52 -0600

    Added the client code

--- lib/qbert/client.rb
+++ lib/qbert/client.rb
@@ -1,6 +1,27 @@
 # frozen_string_literal: true
 
+require "aws-sdk-sqs"
+
 module Qbert
   class Client
+    attr_reader :client, :action_result
+
+    def initialize
+      @client = Aws::SQS::Client.new region: Qbert.configurable.region,
+                                     credentials: Qbert.configurable.credentials
+    end
+
+    def put_message(message = "")
+      @action_result = client.send_message message_hash(message)
+    end
+
+    private
+
+    def message_hash(message)
+      {
+        queue_url: Qbert.configurable.queue_url,
+        message_body: message,
+      }
+    end
   end
 end
